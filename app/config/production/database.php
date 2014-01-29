bb1ccb66e52c227c3be2154278e0ba737195605a
Heinrich de Jongh <heinrichdj@gmail.com>
2014-01-29 16:19:46 +0200

    Basic user login

---
+++ app/config/production/database.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => $_SERVER['DB2_HOST'],
+			'database'  => $_SERVER['DB2_NAME'],
+			'username'  => $_SERVER['DB2_USER'],
+			'password'  => $_SERVER['DB2_PASS'],
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+	),
+);		
